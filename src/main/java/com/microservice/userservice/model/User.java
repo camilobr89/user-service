8a7be2308b42d7224dae818160cd15da582b3ab2
camilobr89 <camilobr89@hotmail.com>
2025-05-13 16:12:40 -0500

    Update .gitignore to include IDE-specific files and build directories

---
+++ src/main/java/com/microservice/userservice/model/User.java
@@ -0,0 +1,25 @@
+package com.microservice.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+    private int age;
+}
+
+
+
